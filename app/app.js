f7f92ab2c0668d069c964f1b06dd01cbc5bc66cc
instablank <instablank.help@gmail.com>
2022-11-20 19:14:46 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,9 @@ const home = require("./src/routes/home");
 app.set("views","./src/views");
 app.set("view engine","ejs");
 
+// __dirname/src/public 폴더를 정적경로로 추가
+app.use(express.static(`${__dirname}/src/public`));
+
 app.use("/",home); 
 //미들웨어를 등록해주는 메서드 routes\home\index.js에서 던진걸 받음
 // /<-루트라는 경로로 접속시 home 호출
