91bf0b6e199cd67499d4e6f0ffe839a90c4b088f
instablank <instablank.help@gmail.com>
2022-11-23 21:43:48 +0900

    회원가입 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,7 +8,9 @@ const ctrl = require("./home.ctrl")
 // 라우팅관련
 router.get("/",ctrl.output.home);
 router.get("/login",ctrl.output.login);
+router.get("/register",ctrl.output.register);
 router.post("/login",ctrl.process.login);
+
 
 // router를 외부 app.js 에서 사용할 수 있도록 외부로 내보내기 
 module.exports = router;
